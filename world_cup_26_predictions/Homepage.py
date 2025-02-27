adc5ae95c7aa26ae1a5bab1808ba4ae7619bf963
Kevin Pfeil <kevinpfeil@Kevins-MacBook-Pro.local>
2025-02-27 12:51:12 -0800

    Updates to webapp.

---
+++ world_cup_26_predictions/Homepage.py
@@ -0,0 +1,53 @@
+import streamlit as st
+import pandas as pd
+import os
+import json
+import streamlit.components.v1 as components
+
+def log_to_console(message: str) -> None:
+    js_code = f"""
+<script>
+    console.log({json.dumps(message)});
+</script>
+"""
+    components.html(js_code)
+
+st.set_page_config(
+    page_title="Homepage",
+    page_icon="👋",
+)
+st.write("# World Cup 2026 Analysis and Predictions ⚽️")
+
+st.sidebar.success("Select an option above..")
+
+
+st.markdown(
+    """
+    The 2026 World Cup is coming to North America! With the US (Seattle included!), Mexico, and Canada all hosting matches
+    it will be an exciting and busy time across the continent. If you are looking for a tool to use to explore who has
+    previously performed well or look to the future and see what some machine learning can predict look no further.
+    The map below shows all of the host cities.
+
+"""
+)
+
+locations = [
+    {'lat': 43.6532, 'lon': -79.3832},
+    {'lat': 49.2827, 'lon': -123.1207},
+    {'lat': 20.6752, 'lon': -103.3473},
+    {'lat': 19.4326, 'lon': -99.1332},
+    {'lat': 25.6866, 'lon': -100.3161},
+    {'lat': 33.7501, 'lon': -84.3885},
+    {'lat': 42.3555, 'lon': -71.0565},
+    {'lat': 32.7767, 'lon': -96.7970},
+    {'lat': 29.7601, 'lon': -95.3701},
+    {'lat': 39.0997, 'lon': -94.5786},
+    {'lat': 34.0549, 'lon': -118.2426},
+    {'lat': 25.7617, 'lon': -80.1918},
+    {'lat': 40.7128, 'lon': -74.0060},
+    {'lat': 39.9526, 'lon': -75.1652},
+    {'lat': 47.6061, 'lon': -122.3328},
+    {'lat': 37.3387, 'lon': -121.8853}
+]
+df = pd.DataFrame(locations)
+st.map(df, zoom=3)
