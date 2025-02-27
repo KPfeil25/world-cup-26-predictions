fd90f47e5cb7bc529b370adf7721a926ce783ca1
Richard Pallangyo <36608703+rapaugustino@users.noreply.github.com>
2025-02-27 14:29:14 -0800

    Update Data Manager and Unit Tests; Remove Unused Folders

--- tests/test_project.py
+++
@@ -1,9 +0,0 @@
-import unittest
-from src.add_nums import add_nums
-
-class TestAddNums(unittest.TestCase):
-    def test_one_shot_case_one(self):
-        num1 = 5
-        num2 = 10
-        result = add_nums(5,10)
-        self.assertEqual(result, 15)
