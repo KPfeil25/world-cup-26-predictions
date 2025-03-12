f0c0f169531b07f96dd78995d0a899318334b808
Richard Pallangyo <36608703+rapaugustino@users.noreply.github.com>
2025-03-11 19:02:39 -0700

    Ensure pylint passes for tests
    I needed to create multiple classes in my tests for pylint not to complain.

--- world_cup_26_predictions/pages/analysis_tool.py
+++ world_cup_26_predictions/pages/analysis_tool.py
@@ -9,7 +9,7 @@ from player_analytics.player_analytics_tab import run_analytics_tab
 def main():
     """
     Main entry point for the World Cup Player Analytics app.
-    Sets up a tabbed interface with a Home tab, Player Analytics tab, and an About tab.
+    Sets up a tabbed interface with a Player Analytics and a Team Analytics tab
     """
 
     st.set_page_config(page_title="World Cup Player Analytics", layout="wide")
@@ -24,6 +24,33 @@ def main():
 
     with tabs[1]:
         st.header("Team Analytics")
+        st.markdown("---")
+        st.subheader("Men & Women World Cup Trivia")
+
+        st.subheader("Men's World Cup")
+        col_a, col_b, col_c, col_d = st.columns(4)
+        with col_a:
+            st.metric("🏟️ First Edition", "1930")
+        with col_b:
+            st.metric("🗓️ Total Tournaments", "22")
+        with col_c:
+            st.metric("🏆 Most Titles", "Brazil (5)")
+        with col_d:
+            st.metric("👑 Current Champion", "Argentina (2022)")
+
+        st.markdown("---")
+        st.subheader("Women's World Cup")
+        col_w1, col_w2, col_w3, col_w4 = st.columns(4)
+        with col_w1:
+            st.metric("🏟️ First Edition", "1991")
+        with col_w2:
+            st.metric("🗓️ Total Tournaments", "9")
+        with col_w3:
+            st.metric("🏆 Most Titles", "USA (4)")
+        with col_w4:
+            st.metric("👑 Current Champion", "Spain (2023)")
+
+        st.markdown("---")
 
 if __name__ == "__main__":
     main()
